a0a373d6ac180005bd85ea5aadb78f0b35b27b2f
sergsagan <sergey.martynuk@gmail.com>
2021-11-10 16:55:31 +0200

    added coin filtering

--- src/App.vue
+++ src/App.vue
@@ -75,9 +75,59 @@
 
       <template v-if="tickers.length">
         <hr class="w-full border-t border-gray-600 my-4" />
+        <div>Фильтр:
+          <input v-model="filter" type="text">
+          <button class="
+            my-4
+            inline-flex
+            items-center
+            mx-2
+            py-2
+            px-4
+            border border-transparent
+            shadow-sm
+            text-sm
+            leading-4
+            font-medium
+            rounded-full
+            text-white
+            bg-gray-600
+            hover:bg-gray-700
+            transition-colors
+            duration-300
+            focus:outline-none
+            focus:ring-2
+            focus:ring-offset-2
+            focus:ring-gray-500
+          ">Назад</button>
+          <button class="
+            my-4
+            inline-flex
+            items-center
+            mx-2
+            py-2
+            px-4
+            border border-transparent
+            shadow-sm
+            text-sm
+            leading-4
+            font-medium
+            rounded-full
+            text-white
+            bg-gray-600
+            hover:bg-gray-700
+            transition-colors
+            duration-300
+            focus:outline-none
+            focus:ring-2
+            focus:ring-offset-2
+            focus:ring-gray-500
+          ">Вперед</button>
+        </div>
+        <hr class="w-full border-t border-gray-600 my-4" />
         <dl class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
           <div
-            v-for="t in tickers"
+            v-for="t in filteredTickers()"
             :key="t.name"
             @click="select(t)"
             :class="{
@@ -191,6 +241,8 @@ export default {
       tickers: [],
       sel: null,
       graph: [],
+      page: 1,
+      filter: ""
     };
   },
 
@@ -206,6 +258,10 @@ export default {
   },
 
   methods: {
+    filteredTickers() {
+      return this.tickers.filter(ticker => ticker.name.includes(this.filter))
+    },
+
     subscribeToUpdates(tickerName) {
       setInterval(async () => {
         const f = await fetch(
@@ -231,6 +287,7 @@ export default {
       };
 
       this.tickers.push(currentTicker);
+      this.filter = "";
 
       localStorage.setItem('crypto-list', JSON.stringify(this.tickers));
       this.subscribeToUpdates(currentTicker.name);
